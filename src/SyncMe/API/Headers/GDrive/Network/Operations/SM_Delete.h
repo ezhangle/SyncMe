d2c9fd0acdfebef443f587fad3badee9b1fc365c
Adrián <arodriba@ull.edu.es>
2016-04-03 03:12:49 +0100

    Google Drive delete operation header finished

--- src/SyncMe/API/Headers/GDrive/Network/Operations/SM_Delete.h
+++ src/SyncMe/API/Headers/GDrive/Network/Operations/SM_Delete.h
@@ -1,4 +1,19 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#ifndef DELETE_H
+#define DELETE_H
 
-#endif // SM_ITEMINFO_H
+#include "network/SM_NetworkManager.h"
+#include "SM_Operation.h"
+#include "gui/SM_Items.h"
+
+class SM_Delete : public SM_NetworkManager, public SM_Operation
+{
+    Q_OBJECT
+public:
+    SM_Delete(QObject *parent = 0);
+    void item(const SM_Items::Data &source);
+
+protected slots:
+    void slotReplyFinished(QNetworkReply*);
+};
+
+#endif // DELETE_H
