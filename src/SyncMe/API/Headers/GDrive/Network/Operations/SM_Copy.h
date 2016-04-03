4ccd70b99a66708b904d2a0bd33a3e337173410f
Adrián <arodriba@ull.edu.es>
2016-04-03 03:13:50 +0100

    Google Drive copy operation header finished

--- src/SyncMe/API/Headers/GDrive/Network/Operations/SM_Copy.h
+++ src/SyncMe/API/Headers/GDrive/Network/Operations/SM_Copy.h
@@ -1,4 +1,27 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#ifndef COPY_H
+#define COPY_H
 
-#endif // SM_ITEMINFO_H
+#include "network/SM_NetworkManager.h"
+#include "SM_Operation.h"
+#include "gui/SM_Items.h"
+
+class SM_Copy : public SM_NetworkManager, public SM_Operation
+{
+    Q_OBJECT
+public:
+    explicit SM_Copy(QObject *parent = 0);
+
+signals:
+    void fileCopied(SM_Items::Data &source);
+
+protected slots:
+    void slotPostFinished(QNetworkReply *reply);
+
+public:
+    void file(const SM_Items::Data &source, const QString &destFolderUrl);
+
+private:
+     SM_Items::Data sourceData;
+};
+
+#endif // COPY_H
