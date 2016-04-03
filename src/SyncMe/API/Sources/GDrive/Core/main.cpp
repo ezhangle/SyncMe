80d23ca9b400ba650a330cc909862bbc39f92923
Adrián <arodriba@ull.edu.es>
2016-04-03 03:32:31 +0100

    main finished for testing the GUI using GDrive engine
    :watch:

--- src/SyncMe/API/Sources/GDrive/Core/main.cpp
+++ src/SyncMe/API/Sources/GDrive/Core/main.cpp
@@ -1,1 +1,19 @@
+#include <QApplication>
+#include "gui/forms/SM_GDrive_MainWindow.h"
+#include <QDesktopWidget>
 
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    SM_GDrive_MainWindow w;
+
+    QRect r(w.geometry());
+
+    r.moveCenter(a.desktop()->availableGeometry().center());
+    w.setGeometry(r);
+
+    w.init();
+    w.show();
+
+    return a.exec();
+}
