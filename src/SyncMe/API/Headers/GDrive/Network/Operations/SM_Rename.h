b5fe63821c6535423a98ac467fdb31f36ec884b1
Adrián <arodriba@ull.edu.es>
2016-04-03 03:11:04 +0100

    Google Drive rename operation header finished

--- src/SyncMe/API/Headers/GDrive/Network/Operations/SM_Rename.h
+++ src/SyncMe/API/Headers/GDrive/Network/Operations/SM_Rename.h
@@ -1,4 +1,21 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#ifndef RENAME_H
+#define RENAME_H
 
-#endif // SM_ITEMINFO_H
+#include "network/SM_NetworkManager.h"
+#include "SM_Operation.h"
+#include "gui/SM_Items.h"
+
+class SM_Rename : public SM_NetworkManager, public SM_Operation
+{
+    Q_OBJECT
+public:
+    SM_Rename(QObject *parent = 0);
+
+protected slots:
+    void slotPutFinished(void);
+
+public:
+    void item(const SM_Items::Data &source, const QString &newName);
+};
+
+#endif // RENAME_H
