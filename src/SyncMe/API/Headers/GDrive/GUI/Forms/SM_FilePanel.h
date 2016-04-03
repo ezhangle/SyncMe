c9a5c9992933827d434e002e06750ea97e93dcd4
Adrián <arodriba@ull.edu.es>
2016-04-03 02:56:27 +0100

    Google Drive file panel header finished

--- src/SyncMe/API/Headers/GDrive/GUI/Forms/SM_FilePanel.h
+++ src/SyncMe/API/Headers/GDrive/GUI/Forms/SM_FilePanel.h
@@ -1,4 +1,46 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#ifndef FILEPANEL_H
+#define FILEPANEL_H
 
-#endif // SM_ITEMINFO_H
+#include <QWidget>
+#include <QTreeWidget>
+#include <QLabel>
+#include "gui/controls/SM_ToolBar.h"
+#include "gui/controls/SM_ComboBox.h"
+
+namespace Ui {
+class FilePanel;
+}
+
+class SM_FilePanel : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit SM_FilePanel(int pn, QWidget *parent = 0);
+    ~SM_FilePanel();
+
+signals:
+    void signalAccountChanged(int panelNum, const QString& accountName);
+
+private slots:
+    void slotActivated(const QString& text);
+
+public:
+    QTreeWidget* getFileView(void) const;
+    QLabel* getPathLabel(void) const;
+    SM_ToolBar* getAccountsToolBar(void) const;
+    SM_ComboBox* getAccountsComboBox(void) const;
+    void fillComboBox(QMap<QString, QString> accountsMap, const QString &currentAccount);
+    int getpanelNum(void) const;
+
+private:
+    void init(void);
+
+private:
+    Ui::FilePanel *ui;
+    SM_ToolBar *accountsToolBar;
+    SM_ComboBox *accountsComboBox;
+    int panelNum;
+};
+
+#endif // FILEPANEL_H
