19c4714402a4f0012bf111045a08924bac352b0b
Adrián <arodriba@ull.edu.es>
2016-04-03 02:56:55 +0100

    Google Drive create folder dialog header finished

--- src/SyncMe/API/Headers/GDrive/GUI/Forms/SM_CreateFolderDialog.h
+++ src/SyncMe/API/Headers/GDrive/GUI/Forms/SM_CreateFolderDialog.h
@@ -1,4 +1,35 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#ifndef CREATEFOLDERDIALOG_H
+#define CREATEFOLDERDIALOG_H
 
-#endif // SM_ITEMINFO_H
+#include <QDialog>
+
+namespace Ui {
+class CreateFolderDialog;
+}
+
+class SM_CreateFolderDialog : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    explicit SM_CreateFolderDialog(QWidget *parent = 0);
+    ~SM_CreateFolderDialog();
+
+public:
+    void setNameLabel();
+
+public slots:
+    void slotAccept();
+    void slotReject();
+    void slotFinished(int result);
+
+signals:
+    void signalAccept(const QString& name);
+    void signalReject(void);
+    void signalFinished(int result);
+
+private:
+    Ui::CreateFolderDialog *ui;
+};
+
+#endif // CREATEFOLDERDIALOG_H
