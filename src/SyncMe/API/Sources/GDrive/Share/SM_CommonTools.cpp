ae07a4e4244e928974ceaa09af0162ce90988f95
Adrián <arodriba@ull.edu.es>
2016-04-03 03:27:49 +0100

    Google Drive common tools class finished

---
+++ src/SyncMe/API/Sources/GDrive/Share/SM_CommonTools.cpp
@@ -0,0 +1,82 @@
+#include "SM_CommonTools.h"
+#include "gui/forms/SM_GDrive_MainWindow.h"
+#include "ui_mainwindow.h"
+#include "share/SM_Defs.h"
+#include "settings/SM_SettingsManager.h"
+#include <QMessageBox>
+#include <QFile>
+#include <QFileInfo>
+#include "share/SM_Debug.h"
+
+void SM_CommonTools::setHeader(const QString &accessToken, QNetworkRequest &request)
+{
+    request.setRawHeader("User-Agent", SM_SettingsManager().applicationName().toLatin1());
+    request.setRawHeader("GData-Version", "3.0");
+    request.setRawHeader("Authorization", QString("Bearer %1").arg(accessToken).toLatin1());
+}
+
+void SM_CommonTools::msg(const QString &text, const QString &title, QWidget *parent)
+{
+    QMessageBox msgBox(parent);
+
+    msgBox.setText(text);
+    msgBox.setWindowTitle(title);
+    msgBox.exec();
+}
+
+int SM_CommonTools::errorMsg(const QString &caption, const QString &text, QWidget *parent)
+{
+    return QMessageBox::critical(parent, caption, text, QMessageBox::Ok);
+}
+
+void SM_CommonTools::logToFile(const QString &fileName, const QByteArray &bytes)
+{
+    QFile file(fileName);
+
+    file.open(QIODevice::WriteOnly);
+    file.write(bytes);
+    file.close();
+}
+
+QByteArray SM_CommonTools::loadFromFile(const QString &fileName)
+{
+    QFile file(fileName);
+
+    file.open(QIODevice::ReadOnly);
+    QByteArray arr = file.readAll();
+    file.close();
+
+    return arr;
+}
+
+bool SM_CommonTools::fileFromURLExists(const QString &url)
+{
+    QFileInfo fi(url);
+    QFile file(fi.fileName());
+
+    return file.exists();
+}
+
+QString SM_CommonTools::getFileNameFromURL(const QString &url)
+{
+  QFileInfo fi(url);
+  return fi.fileName();
+}
+
+QString SM_CommonTools::getIDFromURL(const QString &url)
+{
+    int backParamNum = 1;
+
+    QStringList queryStrs(url.split("/"));
+
+    if(queryStrs[queryStrs.count() - 1].contains(QRegExp("contents")))
+    {
+        backParamNum = 2;
+    }
+
+    QString lastParam(queryStrs[queryStrs.count() - backParamNum]);
+
+    queryStrs = lastParam.split("%3A");
+
+    return queryStrs[queryStrs.count()  - 1];
+}
