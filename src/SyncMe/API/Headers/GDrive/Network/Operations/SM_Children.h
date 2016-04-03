b9b86279b2f770b205948c3a2b9b6672006691ae
Adrián <arodriba@ull.edu.es>
2016-04-03 03:13:32 +0100

    Google Drive children operation header finished

--- src/SyncMe/API/Headers/GDrive/Network/Operations/SM_Children.h
+++ src/SyncMe/API/Headers/GDrive/Network/Operations/SM_Children.h
@@ -1,4 +1,22 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#ifndef CHILDREN_H
+#define CHILDREN_H
 
-#endif // SM_ITEMINFO_H
+#include "network/SM_NetworkManager.h"
+#include "SM_Operation.h"
+#include "gui/SM_Items.h"
+
+class SM_Children : public SM_NetworkManager,  public SM_Operation
+{
+    Q_OBJECT
+public:    
+    SM_Children(QObject *parent = 0);
+
+protected slots:
+     void slotReplyFinished();
+
+public:
+     void fetch();
+
+};
+
+#endif // CHILDREN_H
