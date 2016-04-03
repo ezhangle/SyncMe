ef83cb58f161b3176f03fbc587040ccef9018690
Adrián <arodriba@ull.edu.es>
2016-04-03 16:35:41 +0100

    Get children data op, need JSON fixing

--- src/SyncMe/API/Sources/GDrive/Network/Operations/SM_Children.cpp
+++ src/SyncMe/API/Sources/GDrive/Network/Operations/SM_Children.cpp
@@ -1,4 +1,35 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#include "SM_Children.h"
+#include "settings/SM_SettingsManager.h"
+#include "parsers/SM_JSONParser.h"
+#include "share/SM_Debug.h"
 
-#endif // SM_ITEMINFO_H
+SM_Children::SM_Children(QObject *parent) :
+    SM_NetworkManager(parent)
+{
+}
+
+void SM_Children::fetch()
+{
+    queries.setRawHeader(SM_SettingsManager().accessToken(), request);
+    getRequest(queries.construcChildrenUrl(QString("root")));
+}
+
+void SM_Children::slotReplyFinished()
+{
+    DEBUG << "==========================================================================================================================\n";
+    DEBUG << replyStr;
+    DEBUG << "==========================================================================================================================\n";
+
+    SM_JSONParser jParser;
+    QStringList pathValues;
+
+    pathValues << "items" << "parents";
+    DEBUG << "!!!!" << jParser.getParams(replyStr, pathValues, QString("id"));
+
+    // Hay que arreglar el parser JSON para leer bien los paquetes de Google Drive
+
+    //DEBUG << jParser.getParams(replyStr, pathValues, QString("selfLink"));
+    //DEBUG << jParser.getParams(replyStr, pathValues, QString("childLink"));
+
+    //updatePanelContent(false);
+}
