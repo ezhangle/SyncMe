6d2646e9287ac13c035695625a0ce2db689a2261
Adrián <arodriba@ull.edu.es>
2016-04-03 02:59:10 +0100

    Google Drive GUI spacer header finished

--- src/SyncMe/API/Headers/GDrive/GUI/Controls/SM_Spacer.h
+++ src/SyncMe/API/Headers/GDrive/GUI/Controls/SM_Spacer.h
@@ -1,4 +1,22 @@
-#ifndef SM_TOOLBAR_H
-#define SM_TOOLBAR_H
+#ifndef SPACER_H
+#define SPACER_H
 
-#endif // SM_TOOLBAR_H
+#include "../tools/SM_PainterHelper.h"
+#include <QFrame>
+
+class SM_Spacer : public QFrame
+{
+    Q_OBJECT
+public:
+    explicit SM_Spacer(QWidget *parent, const QString &styleSheet = QString(), bool frame = false);
+    explicit SM_Spacer(QWidget *parent, int width, int height, const QString &styleSheet = QString(), bool frame = false);
+
+protected:
+    void paintEvent(QPaintEvent *event);
+
+private:
+    SM_PainterHelper *painterHelper;
+    bool isFrame;
+};
+
+#endif // SPACER_H
