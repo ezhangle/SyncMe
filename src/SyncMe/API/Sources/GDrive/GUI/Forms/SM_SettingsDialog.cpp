bb30ae2d4395b00835c6be6b5037d3e526cc68f5
Adrián <arodriba@ull.edu.es>
2016-04-03 16:20:50 +0100

    First version of settings dialog finished

--- src/SyncMe/API/Sources/GDrive/GUI/Forms/SM_SettingsDialog.cpp
+++ src/SyncMe/API/Sources/GDrive/GUI/Forms/SM_SettingsDialog.cpp
@@ -1,4 +1,39 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#include "SM_SettingsDialog.h"
+#include "ui_settingsdialog.h"
+#include <QFileDialog>
+#include <QDir>
 
-#endif // SM_ITEMINFO_H
+SM_SettingsDialog::SM_SettingsDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::SettingsDialog)
+{
+    ui->setupUi(this);
+    connect(ui->browseButton, SIGNAL(clicked()), this, SLOT(browseDir()));
+}
+
+SM_SettingsDialog::~SM_SettingsDialog()
+{
+    delete ui;
+}
+
+void SM_SettingsDialog::browseDir()
+{
+    QString dir = QFileDialog::getExistingDirectory(this, tr("Especificar directorio"), ui->directoryLineEdit->text());
+
+    if(dir.isEmpty())
+    {
+        return;
+    }
+
+    ui->directoryLineEdit->setText(dir);
+}
+
+QString SM_SettingsDialog::directoryPath() const
+{
+    return QDir::toNativeSeparators(ui->directoryLineEdit->text());
+}
+
+void SM_SettingsDialog::setDirectoryPath(const QString &dir)
+{
+    ui->directoryLineEdit->setText(QDir::toNativeSeparators(dir));
+}
