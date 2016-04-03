ea502b83046c19787c6bc1edc103ac937b5946c3
Adrián <arodriba@ull.edu.es>
2016-04-03 16:22:34 +0100

    Functional create folder dialog

--- src/SyncMe/API/Sources/GDrive/GUI/Forms/SM_CreateFolderDialog.cpp
+++ src/SyncMe/API/Sources/GDrive/GUI/Forms/SM_CreateFolderDialog.cpp
@@ -1,4 +1,39 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#include "SM_CreateFolderDialog.h"
+#include "ui_createfolderdialog.h"
 
-#endif // SM_ITEMINFO_H
+SM_CreateFolderDialog::SM_CreateFolderDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::CreateFolderDialog)
+{
+    ui->setupUi(this);
+    ui->nameEdit->setFocus();
+
+    connect(ui->buttonBox, SIGNAL(accepted()), this, SLOT(slotAccept()));
+    connect(ui->buttonBox, SIGNAL(rejected()), this, SLOT(slotReject()));
+    connect(this, SIGNAL(finished(int)), this, SLOT(slotFinished(int)));
+}
+
+SM_CreateFolderDialog::~SM_CreateFolderDialog()
+{
+    delete ui;
+}
+
+void SM_CreateFolderDialog::slotAccept()
+{
+    emit signalAccept(ui->nameEdit->text());
+}
+
+void SM_CreateFolderDialog::slotReject()
+{
+    emit signalReject();
+}
+
+void SM_CreateFolderDialog::slotFinished(int result)
+{
+    emit signalFinished(result);
+}
+
+void SM_CreateFolderDialog::setNameLabel()
+{
+    return ui->nameLabel->setText("Crear nueva carpeta:");
+}
