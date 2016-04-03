3c232f3d8e9365e32dbcf85404d7533f3485ec6e
Adrián <arodriba@ull.edu.es>
2016-04-03 03:13:15 +0100

    Google Drive create operation header finished

--- src/SyncMe/API/Headers/GDrive/Network/Operations/SM_Create.h
+++ src/SyncMe/API/Headers/GDrive/Network/Operations/SM_Create.h
@@ -1,4 +1,21 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#ifndef CREATE_H
+#define CREATE_H
 
-#endif // SM_ITEMINFO_H
+#include "network/SM_NetworkManager.h"
+#include "SM_Operation.h"
+#include "gui/SM_Items.h"
+
+class SM_Create : public SM_NetworkManager, public SM_Operation
+{
+    Q_OBJECT
+public:
+    SM_Create(QObject *parent = 0);
+
+protected slots:
+    void slotPostFinished(QNetworkReply* reply);
+
+public:
+    void folder(const QString &name, const QString &parentFolderUrl);
+};
+
+#endif // CREATE_H
