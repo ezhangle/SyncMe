53cd911b1151bc11dea99fa5920c29f73af03c1f
Adrián <arodriba@ull.edu.es>
2016-04-03 02:52:15 +0100

    Google Drive share dialog header finished

--- src/SyncMe/API/Headers/GDrive/GUI/Forms/SM_ShareDialog.h
+++ src/SyncMe/API/Headers/GDrive/GUI/Forms/SM_ShareDialog.h
@@ -1,4 +1,22 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#ifndef SHAREDIALOG_H
+#define SHAREDIALOG_H
 
-#endif // SM_ITEMINFO_H
+#include <QDialog>
+
+namespace Ui {
+class ShareDialog;
+}
+
+class SM_ShareDialog : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    explicit SM_ShareDialog(QWidget *parent = 0);
+    ~SM_ShareDialog();
+    
+private:
+    Ui::ShareDialog *ui;
+};
+
+#endif // SHAREDIALOG_H
