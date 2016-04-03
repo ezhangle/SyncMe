6fe50d71c3dde2bd2ffb55769559ad8af6bb11fa
Adrián <arodriba@ull.edu.es>
2016-04-03 02:53:04 +0100

    Google Drive progress bar dialog header finished

--- src/SyncMe/API/Headers/GDrive/GUI/Forms/SM_ProgressBarDialog.h
+++ src/SyncMe/API/Headers/GDrive/GUI/Forms/SM_ProgressBarDialog.h
@@ -1,4 +1,32 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#ifndef PROGRESSBARDIALOG_H
+#define PROGRESSBARDIALOG_H
 
-#endif // SM_ITEMINFO_H
+#include <QDialog>
+
+namespace Ui {
+class progressBarDialog;
+}
+
+class SM_ProgressBarDialog : public QDialog
+{
+    Q_OBJECT   
+public:
+    explicit SM_ProgressBarDialog(QWidget *parent = 0);
+    ~SM_ProgressBarDialog();
+
+public:
+    void setMaximum(qint64 max);
+    void setValue(qint64 val);
+    void setText(const QString& text);
+
+public slots:
+    void slotCancel();
+
+signals:
+    void signalProgressCanceled();
+    
+private:
+    Ui::progressBarDialog *ui;
+};
+
+#endif // PROGRESSBARDIALOG_H
