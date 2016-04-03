0ebd360a8e28ca1d94efaa4c82cad1aaa1c96c87
Adrián <arodriba@ull.edu.es>
2016-04-03 16:23:36 +0100

    Tool Bar controller finished

--- src/SyncMe/API/Sources/GDrive/GUI/Controls/SM_ToolBar.cpp
+++ src/SyncMe/API/Sources/GDrive/GUI/Controls/SM_ToolBar.cpp
@@ -1,4 +1,27 @@
-#ifndef SM_TOOLBAR_H
-#define SM_TOOLBAR_H
+#include "SM_ToolBar.h"
+#include <QLayout>
+#include <QStyleOption>
+#include <QPainter>
 
-#endif // SM_TOOLBAR_H
+SM_ToolBar::SM_ToolBar(QWidget *parent, int height) :
+    QToolBar(parent)
+{ 
+    setStyleSheet("QToolBar {background-color: qlineargradient(spread:pad, x1:1, y1:0.432, x2:1, y2:0, stop:0 rgba(88, 107, 113, 255), stop:1 rgba(88, 107, 113, 255));}");
+    setFixedHeight(height);
+
+    const int offset = 0;
+
+    layout()->setContentsMargins(offset, offset, offset, offset);
+}
+
+void SM_ToolBar::paintEvent(QPaintEvent *event)
+{    
+    Q_UNUSED(event);
+
+    QStyleOption opt;
+    QPainter p(this);
+
+    opt.init(this);
+
+    style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
+}
