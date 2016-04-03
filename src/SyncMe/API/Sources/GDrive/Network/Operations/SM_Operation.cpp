fc8d245d37474958ed4e20a8c562c5102f46493c
Adrián <arodriba@ull.edu.es>
2016-04-03 16:33:19 +0100

    Operation base class :clap:

--- src/SyncMe/API/Sources/GDrive/Network/Operations/SM_Operation.cpp
+++ src/SyncMe/API/Sources/GDrive/Network/Operations/SM_Operation.cpp
@@ -1,4 +1,13 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#include "SM_Operation.h"
+#include "core/SM_DriveEngine.h"
+#include "network/SM_ContentManager.h"
 
-#endif // SM_ITEMINFO_H
+SM_Operation::SM_Operation()
+{
+}
+
+void SM_Operation::updatePanelContent(bool opposite)
+{
+    SM_ContentManager* cm(SDriveEngine::inst()->getContentMngr(opposite));
+    cm->get(cm->getParentFolderUrl());
+}
