97842f47d04ae09124543a81ea47d0d775cb9580
Adrián <arodriba@ull.edu.es>
2016-04-03 16:23:57 +0100

    Spacer GUI element finished

--- src/SyncMe/API/Sources/GDrive/GUI/Controls/SM_Spacer.cpp
+++ src/SyncMe/API/Sources/GDrive/GUI/Controls/SM_Spacer.cpp
@@ -1,4 +1,33 @@
-#ifndef SM_TOOLBAR_H
-#define SM_TOOLBAR_H
+#include "SM_Spacer.h"
 
-#endif // SM_TOOLBAR_H
+SM_Spacer::SM_Spacer(QWidget *parent, const QString &styleSheet, bool frame) :
+    QFrame(parent),
+    isFrame(frame)
+{
+    setVisible(true);
+    setStyleSheet(styleSheet);
+    setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+}
+
+SM_Spacer::SM_Spacer(QWidget *parent, int width, int height, const QString &styleSheet, bool frame) :
+    QFrame(parent),
+    isFrame(frame)
+{
+    setVisible(true);
+    setStyleSheet(styleSheet);
+
+    setFixedWidth(width);
+    setFixedHeight(height);
+}
+
+void SM_Spacer::paintEvent(QPaintEvent *event)
+{
+    if(!isFrame) return;
+
+    Q_UNUSED(event)
+
+    #ifndef Q_OS_MAC
+        SM_PainterHelper painterHelper(this);
+        painterHelper.drawFrame(rect(), QColor(88, 107, 113));
+    #endif
+}
