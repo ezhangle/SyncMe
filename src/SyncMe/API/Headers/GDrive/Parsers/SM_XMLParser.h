b9922186161b2c519d048c85c34af0ef3f42fdf6
Adrián <arodriba@ull.edu.es>
2016-04-03 03:17:16 +0100

    Google Drive XML parser header finished

--- src/SyncMe/API/Headers/GDrive/Parsers/SM_XMLParser.h
+++ src/SyncMe/API/Headers/GDrive/Parsers/SM_XMLParser.h
@@ -1,4 +1,24 @@
-#ifndef SM_XMLPARSER_H
-#define SM_XMLPARSER_H
+#ifndef XMLPARSER_H
+#define XMLPARSER_H
 
-#endif // SM_XMLPARSER_H
+#include <QXmlDefaultHandler>
+#include "SM_XMLHandler.h"
+
+class SM_XMLParser : public QXmlDefaultHandler
+{
+public:
+    SM_XMLParser();
+    ~SM_XMLParser();
+
+public:
+    bool startElement(const QString &qName, const QXmlAttributes &attribs);
+    bool endElement();
+    bool characters(const QString &str);
+    bool fatalError(const QXmlParseException &exception);
+    SM_XMLHandler* getXMLHandler(void) const;
+
+private:
+    QScopedPointer<SM_XMLHandler> xmlHandler;
+};
+
+#endif // XMLPARSER_H
