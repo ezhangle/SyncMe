558831f62948862004eb9869a521f189edc2eaad
Adrián <arodriba@ull.edu.es>
2016-04-03 03:20:31 +0100

    Google Drive template singleton class finished

--- src/SyncMe/API/Headers/GDrive/Share/SM_TSingleton_Item.h
+++ src/SyncMe/API/Headers/GDrive/Share/SM_TSingleton_Item.h
@@ -1,4 +1,47 @@
-#ifndef SM_SINGLETON_H
-#define SM_SINGLETON_H
+#ifndef SINGLETON_H
+#define SINGLETON_H
 
-#endif // SM_SINGLETON_H
+#include <QObject>
+
+template <typename T>
+class SM_TSingleton_Item
+{
+public:
+    static T* inst(void);
+    static T* inst(QWidget *parent);
+    static void freeInst(void);
+
+private:
+    SM_TSingleton_Item(){}
+    ~SM_TSingleton_Item(){}
+    Q_DISABLE_COPY(SM_TSingleton_Item)
+
+private:
+    static T* instObject;
+};
+
+template <typename T>
+T* SM_TSingleton_Item<T>::instObject = NULL;
+
+template <typename T>
+T* SM_TSingleton_Item<T>::inst(void)
+{
+    if(!instObject) instObject = new T;
+    return instObject;
+}
+
+template <typename T>
+T* SM_TSingleton_Item<T>::inst(QWidget *parent)
+{
+    if(!instObject) instObject = new T(parent);
+    return instObject;
+}
+
+template <typename T>
+void SM_TSingleton_Item<T>::freeInst(void)
+{
+    if(instObject) delete instObject;
+    instObject = NULL;
+}
+
+#endif // SINGLETON_H
