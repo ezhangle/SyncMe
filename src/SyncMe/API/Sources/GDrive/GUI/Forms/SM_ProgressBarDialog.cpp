2a5d1a6c8f4fd1fd89bc41bdc76c86ec1f995d2f
Adrián <arodriba@ull.edu.es>
2016-04-03 16:21:18 +0100

    Progress bar dialog working (version 1)

--- src/SyncMe/API/Sources/GDrive/GUI/Forms/SM_ProgressBarDialog.cpp
+++ src/SyncMe/API/Sources/GDrive/GUI/Forms/SM_ProgressBarDialog.cpp
@@ -1,4 +1,36 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#include "SM_ProgressBarDialog.h"
+#include "ui_progressbardialog.h"
 
-#endif // SM_ITEMINFO_H
+SM_ProgressBarDialog::SM_ProgressBarDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::progressBarDialog)
+{
+    ui->setupUi(this);
+    connect(ui->cancelButton, SIGNAL(clicked()), this, SLOT(slotCancel()));
+}
+
+SM_ProgressBarDialog::~SM_ProgressBarDialog()
+{
+    delete ui;
+}
+
+void SM_ProgressBarDialog::slotCancel()
+{
+    hide();
+    emit signalProgressCanceled();
+}
+
+void SM_ProgressBarDialog::setMaximum(qint64 max)
+{
+ ui->progressBar->setMaximum(max);
+}
+
+void SM_ProgressBarDialog::setValue(qint64 val)
+{
+  ui->progressBar->setValue(val);
+}
+
+void SM_ProgressBarDialog::setText(const QString& text)
+{
+  ui->infoLbel->setText(text);
+}
