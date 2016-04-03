1938747b739ae9e5d28017648cd6c49501bf910d
Adrián <arodriba@ull.edu.es>
2016-04-03 03:19:29 +0100

    Google Drive debug helper finished

--- src/SyncMe/API/Headers/GDrive/Share/SM_Debug.h
+++ src/SyncMe/API/Headers/GDrive/Share/SM_Debug.h
@@ -1,4 +1,12 @@
-#ifndef SM_DEBUG_H
-#define SM_DEBUG_H
+#ifndef DEBUG_H
+#define DEBUG_H
 
-#endif // SM_DEBUG_H
+#include <QDebug>
+
+#ifdef Q_WS_WIN
+   #define DEBUG qDebug() << "[ DEBUG:" << __PRETTY_FUNCTION__ << ", line:" << __LINE__ << "]"
+#else
+   #define DEBUG qDebug() << "[ DEBUG:" << __FUNCTION__ << ", line:" << __LINE__ << "]"
+#endif
+
+#endif // DEBUG_H
