d5df24e9f7a8b08a3b13418dbb34ba14c31c72c0
Adrián <arodriba@ull.edu.es>
2016-04-03 16:32:23 +0100

    Rename operation first version :+1:

--- src/SyncMe/API/Sources/GDrive/Network/Operations/SM_Rename.cpp
+++ src/SyncMe/API/Sources/GDrive/Network/Operations/SM_Rename.cpp
@@ -1,4 +1,19 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#include "SM_Rename.h"
+#include "settings/SM_SettingsManager.h"
 
-#endif // SM_ITEMINFO_H
+SM_Rename::SM_Rename(QObject *parent) :
+    SM_NetworkManager(parent)
+{
+}
+
+void SM_Rename::item(const SM_Items::Data &source, const QString &newName)
+{
+    postData = queries.getRenameFileData(newName);
+    queries.setRawHeader(SM_SettingsManager().accessToken(), request);
+    putRequest(QUrl(queries.constructRenameFileUrl(source.self)));
+}
+
+void SM_Rename::slotPutFinished(void)
+{
+    updatePanelContent(false);
+}
