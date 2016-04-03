e488279636206be980b7b4c124fb00248725be30
Adrián <arodriba@ull.edu.es>
2016-04-03 03:06:17 +0100

    Google Drive download file manager header finished

--- src/SyncMe/API/Headers/GDrive/Network/SM_DownloadFileManager.h
+++ src/SyncMe/API/Headers/GDrive/Network/SM_DownloadFileManager.h
@@ -1,4 +1,22 @@
-#ifndef SM_DOWNLOADMANAGER_H
-#define SM_DOWNLOADMANAGER_H
+#ifndef DOWNLOADMANAGER_H
+#define DOWNLOADMANAGER_H
 
-#endif // SM_DOWNLOADMANAGER_H
+#include "share/SM_CommonTools.h"
+#include "SM_NetworkManager.h"
+
+class SM_DownloadFileManager : public SM_NetworkManager
+{
+    Q_OBJECT
+public:
+    explicit SM_DownloadFileManager(QObject *parent = 0);
+
+public:
+    void startDownload(QUrl url, QString &fileName, const QString &type = "text/html");
+
+protected slots:
+    virtual void slotDownloadProgress(qint64 bytesReceived, qint64 bytesTotal);
+    virtual void slotDownloadFinished();
+    virtual void slotDownloadReadyRead();
+};
+
+#endif // DOWNLOADMANAGER_H
