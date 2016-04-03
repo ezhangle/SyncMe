ddfe7b0f6446fd310a9ed2a43ad48e5739c713cb
Adrián <arodriba@ull.edu.es>
2016-04-03 02:52:41 +0100

    Google Drive settings dialog header finished

--- src/SyncMe/API/Headers/GDrive/GUI/Forms/SM_SettingsDialog.h
+++ src/SyncMe/API/Headers/GDrive/GUI/Forms/SM_SettingsDialog.h
@@ -1,4 +1,28 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#ifndef SETTINGS_DIALOG_H
+#define SETTINGS_DIALOG_H
 
-#endif // SM_ITEMINFO_H
+#include <QDialog>
+
+namespace Ui {
+    class SettingsDialog;
+}
+
+class SM_SettingsDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit SM_SettingsDialog(QWidget *parent = 0);
+    ~SM_SettingsDialog();
+
+    QString directoryPath() const;
+    void setDirectoryPath(const QString& dir);
+
+private slots:
+    void browseDir();
+
+private:
+    Ui::SettingsDialog *ui;
+};
+
+#endif // SETTINGS_DIALOG_H
