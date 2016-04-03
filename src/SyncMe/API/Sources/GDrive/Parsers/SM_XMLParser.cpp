6f9ad342844c504d799815eb6d1ea5327a356d81
Adrián <arodriba@ull.edu.es>
2016-04-03 03:29:19 +0100

    Google Drive XML parser class finished

--- src/SyncMe/API/Sources/GDrive/Parsers/SM_XMLParser.cpp
+++ src/SyncMe/API/Sources/GDrive/Parsers/SM_XMLParser.cpp
@@ -1,1 +1,37 @@
+#include "SM_XMLParser.h"
 
+SM_XMLParser::SM_XMLParser():
+    xmlHandler(new SM_XMLHandler)
+{
+}
+
+SM_XMLParser::~SM_XMLParser()
+{
+}
+
+bool SM_XMLParser::startElement(const QString &qName, const QXmlAttributes &attribs)
+{
+    //return xmlHandler->startElement(namespaceURI, localName, qName, attribs);
+    return xmlHandler->startElement(qName, attribs);
+}
+
+bool SM_XMLParser::endElement()
+{
+    //return xmlHandler->endElement(namespaceURI, localName, qName);
+    return xmlHandler->endElement();
+}
+
+bool SM_XMLParser::characters(const QString &str)
+{
+    return xmlHandler->characters(str);
+}
+
+bool SM_XMLParser::fatalError(const QXmlParseException &exception)
+{
+    return xmlHandler->fatalError(exception);
+}
+
+SM_XMLHandler* SM_XMLParser::getXMLHandler(void) const
+{
+    return xmlHandler.data();
+}
