29cf0a06103f9ca3e888c2a29162928e9459ce70
Adrián <arodriba@ull.edu.es>
2016-04-03 02:38:22 +0100

    Google Drive engine header finished

--- src/SyncMe/API/Headers/GDrive/Core/SM_DriveEngine.h
+++ src/SyncMe/API/Headers/GDrive/Core/SM_DriveEngine.h
@@ -1,1 +1,59 @@
-//
+#ifndef DRIVEENGINE_H
+#define DRIVEENGINE_H
+
+#include <QTreeWidgetItem>
+#include "network/SM_DownloadFileManager.h"
+#include "network/SM_UploadFileManager.h"
+#include "network/SM_ContentManager.h"
+#include "gui/forms/SM_GDrive_MainWindow.h"
+#include "ui_mainwindow.h"
+#include "gui/SM_FilesTransferUI.h"
+#include "gui/SM_ContentUI.h"
+#include "gui/SM_CheckUI.h"
+#include "share/SM_Enums.h"
+#include "gui/forms/SM_FilePanel.h"
+
+class SM_FilesTransferUI;
+class SM_ContentUI;
+
+class SM_DriveEngine : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SM_DriveEngine(QWidget *p = 0);
+    ~SM_DriveEngine();
+
+    friend class SM_FilesTransferUI;
+    friend class SM_ContentUI;
+
+public:   
+    SM_CheckUI *getCheckUI(void) const;
+    SM_ContentManager *getContentMngr(bool opposite = false) const;
+    SM_FilesTransferUI *getfilesTransferUI(void) const;
+    SM_ContentUI *getfilesUI(void) const;
+    QWidget *getParent(void) const;
+    void init(void);
+    SM_FilePanel *getFilePanel(SM_GUI_EPanels panel) const;
+    void updatePanel(int panelId, bool initLoad);
+
+private slots:
+    void slotFirstPanelIsLoaded(void);
+    void slotAccountChanged(int panelNum, const QString& accountName);
+
+private:
+    void reset(void);   
+
+private:
+    QWidget *parent;
+    QScopedPointer<SM_CheckUI> checkUI;
+    QScopedPointer<SM_DownloadFileManager> downloadMngr;
+    QScopedPointer<SM_ContentManager> contentMngr[EPanelsCount];
+    QScopedPointer<SM_FilesTransferUI> filesTransferUI;
+    QScopedPointer<SM_ContentUI> filesUI;
+    QScopedPointer<SM_UploadFileManager> uploadFileMngr;
+    SM_FilePanel *filesViews[EPanelsCount];
+};
+
+typedef SM_TSingleton_Item<SM_DriveEngine> SDriveEngine;
+
+#endif // DRIVEENGINE_H
