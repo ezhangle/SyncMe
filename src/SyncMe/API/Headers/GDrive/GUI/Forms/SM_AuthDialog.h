1a5195d3cd04d3ac77df922a46f65a7e53fb9aa0
Adrián <arodriba@ull.edu.es>
2016-04-03 02:57:17 +0100

    Google Drive auth dialog header finished

--- src/SyncMe/API/Headers/GDrive/GUI/Forms/SM_AuthDialog.h
+++ src/SyncMe/API/Headers/GDrive/GUI/Forms/SM_AuthDialog.h
@@ -1,4 +1,34 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#ifndef AUTHDIALOG_H
+#define AUTHDIALOG_H
 
-#endif // SM_ITEMINFO_H
+#include <QDialog>
+#include "network/SM_Auth.h"
+
+namespace Ui {
+class AuthDialog;
+}
+
+class SM_AuthDialog : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    explicit SM_AuthDialog(QWidget *parent = 0);
+    ~SM_AuthDialog();
+
+signals:
+    void signalTokens(const QString &accessToken, const QString &refreshToken);
+
+private slots:
+    void slotUrlChanged(const QUrl& url);
+    void slotAuthResponse(const QString &accessToken, const QString &refreshToken);
+
+private:
+    void init(void);
+    
+private:
+    Ui::AuthDialog *ui;
+    QScopedPointer<SM_Auth> auth;
+};
+
+#endif // AUTHDIALOG_H
