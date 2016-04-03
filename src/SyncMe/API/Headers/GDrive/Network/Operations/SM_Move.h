b8b791f93263458cf2b80abf9000ec87cdc3523e
Adrián <arodriba@ull.edu.es>
2016-04-03 03:12:07 +0100

    Google Drive move operation header finished

--- src/SyncMe/API/Headers/GDrive/Network/Operations/SM_Move.h
+++ src/SyncMe/API/Headers/GDrive/Network/Operations/SM_Move.h
@@ -1,4 +1,25 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#ifndef MOVE_H
+#define MOVE_H
 
-#endif // SM_ITEMINFO_H
+#include <QObject>
+#include "network/operations/SM_Copy.h"
+#include "network/operations/SM_Delete.h"
+
+class SM_Move : public QObject
+{
+    Q_OBJECT
+public:
+    SM_Move(QObject* parent = 0);
+
+public:
+    void item(const SM_Items::Data &source, const QString &destFolderUrl);
+
+private slots:
+    void slotFileCopied(SM_Items::Data &source);
+
+private:
+    SM_Copy copy;
+    SM_Delete del;
+};
+
+#endif // MOVE_H
