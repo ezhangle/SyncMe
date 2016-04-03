8b3f849b09911c500ef4ab1a643d455218fc6df9
Adrián <arodriba@ull.edu.es>
2016-04-03 02:43:07 +0100

    Google Drive files transfer UI header finished

--- src/SyncMe/API/Headers/GDrive/GUI/SM_FilesTransferUI.h
+++ src/SyncMe/API/Headers/GDrive/GUI/SM_FilesTransferUI.h
@@ -1,4 +1,22 @@
-#ifndef SM_FILESTRANSFERUI_H
-#define SM_FILESTRANSFERUI_H
+#ifndef FILESTRANSFER_H
+#define FILESTRANSFER_H
 
-#endif // SM_FILESTRANSFERUI_H
+#include "core/SM_DriveEngine.h"
+
+class SM_FilesTransferUI : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SM_FilesTransferUI(QObject *parent = 0);
+
+public:
+    void upload(void);
+    void download(void);
+
+private slots:
+    void slotDownload(void);
+    void slotUpload(void);
+
+};
+
+#endif // FILESTRANSFER_H
