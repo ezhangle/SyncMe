7d01d54b1658a7ae78ff6bb11c7e92f3a35546f8
Adrián <arodriba@ull.edu.es>
2016-04-03 03:04:31 +0100

    Google Drive upload file manager header finished

--- src/SyncMe/API/Headers/GDrive/Network/SM_UploadFileManager.h
+++ src/SyncMe/API/Headers/GDrive/Network/SM_UploadFileManager.h
@@ -1,4 +1,36 @@
-#ifndef SM_UPLOADMANAGER_H
-#define SM_UPLOADMANAGER_H
+#ifndef UPLOADMANAGER_H
+#define UPLOADMANAGER_H
 
-#endif // SM_UPLOADMANAGER_H
+#include "share/SM_CommonTools.h"
+#include "SM_NetworkManager.h"
+#include <QFileInfo>
+
+class SM_UploadFileManager : public SM_NetworkManager
+{
+    Q_OBJECT
+public:  
+    explicit SM_UploadFileManager(QObject *parent = 0);
+
+signals:
+    void signalUpdateFileList();
+
+public slots:
+    virtual void slotUploadFinished();
+    virtual void slotUploadProgress(qint64 bytesSent, qint64 bytesTotal);
+
+public:
+    void setUploadSettings(void);
+    void startUpload(QUrl url, const QString &fileName);
+
+protected:
+    void setProgressBarSettings(const QString &fileName, const QString &progressBarDialogInfoText);
+    void postFinishedActions(QNetworkReply* reply);
+
+private:
+    QString getContentTypeByExtension(const QString &ext);
+
+private:
+    bool allowPut;
+};
+
+#endif // UPLOADMANAGER_H
