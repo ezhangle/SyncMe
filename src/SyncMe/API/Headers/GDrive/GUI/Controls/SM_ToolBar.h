3d516b9085c8f1d49a628607485b640777b5c272
Adrián <arodriba@ull.edu.es>
2016-04-03 02:58:31 +0100

    Google Drive tool bar header finished

--- src/SyncMe/API/Headers/GDrive/GUI/Controls/SM_ToolBar.h
+++ src/SyncMe/API/Headers/GDrive/GUI/Controls/SM_ToolBar.h
@@ -1,4 +1,16 @@
-#ifndef SM_TOOLBAR_H
-#define SM_TOOLBAR_H
+#ifndef TOOLBAR_H
+#define TOOLBAR_H
 
-#endif // SM_TOOLBAR_H
+#include <QToolBar>
+
+class SM_ToolBar : public QToolBar
+{
+    Q_OBJECT
+public:
+    explicit SM_ToolBar(QWidget *parent = 0, int height = 26);
+    
+protected:
+    void paintEvent(QPaintEvent *event);
+};
+
+#endif // TOOLBAR_H
