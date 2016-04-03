26dd39819ea8af7fee0d4008109a43758318fb9d
Adrián <arodriba@ull.edu.es>
2016-04-03 02:49:50 +0100

    Google Drive GUI painter helper header finished

--- src/SyncMe/API/Headers/GDrive/GUI/Tools/SM_PainterHelper.h
+++ src/SyncMe/API/Headers/GDrive/GUI/Tools/SM_PainterHelper.h
@@ -1,4 +1,19 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#ifndef PAINTERHELPER_H
+#define PAINTERHELPER_H
 
-#endif // SM_ITEMINFO_H
+#include <QPainter>
+#include <QPen>
+
+class SM_PainterHelper : public QPainter
+{
+public:
+    SM_PainterHelper(QPaintDevice *device);
+
+public:
+    void drawFrame(QRect rect, QColor color);
+
+private:
+     QPen pen;
+};
+
+#endif // PAINTERHELPER_H
