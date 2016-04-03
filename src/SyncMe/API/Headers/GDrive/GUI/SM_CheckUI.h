828d1e9b021ea32dc65b0799290c9dec08fae2c6
Adrián <arodriba@ull.edu.es>
2016-04-03 02:42:02 +0100

    Google Drive check UI header finished

--- src/SyncMe/API/Headers/GDrive/GUI/SM_CheckUI.h
+++ src/SyncMe/API/Headers/GDrive/GUI/SM_CheckUI.h
@@ -1,4 +1,19 @@
-#ifndef SM_CHECKUI_H
-#define SM_CHECKUI_H
+#ifndef CHECK_H
+#define CHECK_H
 
-#endif // SM_CHECKUI_H
+#include <QObject>
+
+class SM_CheckUI: public QObject
+{
+    Q_OBJECT
+public:
+     explicit SM_CheckUI(QObject *parent = 0);
+
+public:
+    bool checkReg(void);
+
+public slots:
+    bool slotCheckWorkDir(bool showDlg = true);
+};
+
+#endif // CHECK_H
