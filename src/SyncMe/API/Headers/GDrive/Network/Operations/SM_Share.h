7f62b42d1649de998b1d11301e32cea03a96df1e
Adrián <arodriba@ull.edu.es>
2016-04-03 03:10:21 +0100

    Google Drive share operation header finished

--- src/SyncMe/API/Headers/GDrive/Network/Operations/SM_Share.h
+++ src/SyncMe/API/Headers/GDrive/Network/Operations/SM_Share.h
@@ -1,4 +1,17 @@
-#ifndef SM_ITEMINFO_H
-#define SM_ITEMINFO_H
+#ifndef SHARE_H
+#define SHARE_H
 
-#endif // SM_ITEMINFO_H
+#include "network/SM_NetworkManager.h"
+#include "gui/SM_Items.h"
+
+class SM_Share : public SM_NetworkManager
+{
+    Q_OBJECT
+public:
+    SM_Share(QObject *parent = 0);
+
+public:
+    void file();
+};
+
+#endif // SHARE_H
